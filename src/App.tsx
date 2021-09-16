145ad2a3fdf10899ea8d520e8112592e45bf5a20
Angelo Huang <huangelo02@gmail.com>
2021-09-16 12:07:28 +0200

    feat: create landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import LandingPage from "./Components/LandingPage";
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <LandingPage/>
     </div>
   );
 }
