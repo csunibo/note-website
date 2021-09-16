dd5c00776a2e53bac50f22b9d0855df766b0e0b9
Angelo Huang <huangelo02@gmail.com>
2021-09-16 11:01:40 +0200

    feat: create navbar

---
+++ src/styles/MainStyles.tsx
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useClasses = makeStyles(() => ({
+        flexGrow_1: {
+            flexGrow: 1
+        }
+    }));
+
+export default useClasses
